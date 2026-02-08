bb6251c412a1397a0fb0d70bb8e1a0984b4f2be6
dub dub Wubba lubba <tonyhsieh606@gmail.com>
2026-02-08 17:49:21 +0800

    update: Sovereign standards

---
+++ src/main/java/com/audit/global/SovereignCrypto.java
@@ -0,0 +1,9 @@
+package com.audit.global;
+public class SovereignCrypto {
+    public void audit() {
+        String china = "SM2";
+        String russia = "GOST-3411";
+        String germany = "BrainpoolP256r1";
+        System.out.println("Auditing regional standards...");
+    }
+}
