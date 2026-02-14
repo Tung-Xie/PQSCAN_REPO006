b197584fc39eea68e5246dc75c41142e592a7379
Tung-Xie <tony.xie@chelpis.com>
2026-02-14 22:09:45 +0800

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from python_pqc.migration.quantum_provider import QuantumProvider
-
-if __name__ == "__main__":
-    print("Executing PQSCAN REPO 006 Validation Suite...")
-    qp = QuantumProvider()
-    qp.boot_pqc()
